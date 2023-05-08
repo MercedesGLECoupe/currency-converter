4022cfd8f072484a43d0cacea53f0b57ae8cf518
PawelSI <pawel.sitkowski@hotmail.com>
2023-05-08 11:18:09 +0200

    added functions

--- currency-converter.js
+++ currency-converter.js
@@ -1,96 +1,114 @@
-
-let currencyOption = document.querySelector(".js-select");
-let currencyConverter = document.querySelector(".js-pln");
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
+{
 
 
-let PLN = 1;
-let EUR = 4.63;
-let CHF = 4.70;
-let GBP = 5.25;
-let USD = 4.22;
-let CNY = 0.61;
-let CZK = 0.20;
-let HUF = 0.01;
-let UAH = 0.11;
-let THB = 0.12;
-let TRY = 0.22;
-let ISK = 0.03;
-let NOK = 0.40;
-let DKK = 0.62;
-let SEK = 0.41;
-let ILS = 1.16;
-let RON = 0.94;
-let MXN = 0.23;
-let ZAR = 0.23;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const calculateResult = (amount, currency) => {
 
-    let currency = currencyOption.value;
-    let amount = +currencyConverter.value;
-    let result;
+        const PLN = 1;
+        const EUR = 4.63;
+        const CHF = 4.70;
+        const GBP = 5.25;
+        const USD = 4.22;
+        const CNY = 0.61;
+        const CZK = 0.20;
+        const HUF = 0.01;
+        const UAH = 0.11;
+        const THB = 0.12;
+        const TRY = 0.22;
+        const ISK = 0.03;
+        const NOK = 0.40;
+        const DKK = 0.62;
+        const SEK = 0.41;
+        const ILS = 1.16;
+        const RON = 0.94;
+        const MXN = 0.23;
+        const ZAR = 0.23;
 
-    switch (currency) {
-        case "EUR":
-            result = amount / EUR;
-            break;
-        case "CHF":
-            result = amount / CHF;
-            break;
-        case "GBP":
-            result = amount / GBP;
-            break;
-        case "USD":
-            result = amount / USD;
-            break
-        case "CNY":
-            result = amount / CNY;
-            break
-        case "CZK":
-            result = amount / CZK;
-            break
-        case "HUF":
-            result = amount / HUF;
-            break
-        case "UAH":
-            result = amount / UAH;
-            break
-        case "THB":
-            result = amount / THB;
-            break
-        case "TRY":
-            result = amount / TRY;
-            break
-        case "ISK":
-            result = amount / ISK;
-            break
-        case "NOK":
-            result = amount / NOK;
-            break
-        case "DKK":
-            result = amount / DKK;
-            break
-        case "SEK":
-            result = amount / SEK;
-            break
-        case "ILS":
-            result = amount / ILS;
-            break
-        case "RON":
-            result = amount / RON;
-            break
-        case "MXN":
-            result = amount / MXN;
-            break
-        case "ZAR":
-            result = amount / ZAR;
-            break
+        switch (currency) {
+            case "EUR":
+                return amount / EUR;
+
+            case "CHF":
+                return amount / CHF;
+
+            case "GBP":
+                return amount / GBP;
+
+            case "USD":
+                return amount / USD;
+
+            case "CNY":
+                return amount / CNY;
+
+            case "CZK":
+                return amount / CZK;
+
+            case "HUF":
+                return amount / HUF;
+
+            case "UAH":
+                return amount / UAH;
+
+            case "THB":
+                return amount / THB;
+
+            case "TRY":
+                return amount / TRY;
+
+            case "ISK":
+                return amount / ISK;
+
+            case "NOK":
+                return amount / NOK;
+
+            case "DKK":
+                return amount / DKK;
+
+            case "SEK":
+                return amount / SEK;
+
+            case "ILS":
+                return amount / ILS;
+
+            case "RON":
+                return amount / RON;
+
+            case "MXN":
+                return amount / MXN;
+
+            case "ZAR":
+                return amount / ZAR;
+
+        }
     }
 
+    const updateResultText = (amount, result, currency) => {
+        const returnElement = document.querySelector(".js-result");
+        returnElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)
+            } ${currency}`;
+    }
 
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
-});
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const currencyOption = document.querySelector(".js-select");
+        const currencyConverter = document.querySelector(".js-pln");
+        
 
 
+        const amount = +currencyConverter.value
+        const currency = currencyOption.value
+
+        const result = calculateResult(amount, currency);
+
+        updateResultText(amount, result, currency);
+    }
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    }
+
+    init();
+}
